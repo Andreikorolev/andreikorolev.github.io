942d1df74811a160df5cad6bc515bca88752aa0e
Andrei Korolev <andreikoroleffs@gmail.com>
2023-11-24 20:44:12 +0300

    end of day commit

---
+++ quiz-data.js
@@ -0,0 +1,46 @@
+const quizData = [
+  {
+    typeOfInput: "radio",
+    cardLabel: "CARD 1 LABEL",
+    cardName: "card1",
+    questions: [
+      {label: "LABEL1", value: 1},
+      {label: "LABEL2", value: 2},
+      {label: "LABEL3", value: 3},
+      {label: "LABEL4", value: 4},
+    ],
+  },
+  {
+    typeOfInput: "checkbox",
+    cardLabel: "CARD 2 LABEL",
+    cardName: "card2",
+    questions: [
+      {label: "LABEL1", value: 1},
+      {label: "LABEL2", value: 2},
+      {label: "LABEL3", value: 3},
+      {label: "LABEL4", value: 4},
+    ],
+  },
+  {
+    typeOfInput: "radio",
+    cardLabel: "CARD 3 LABEL",
+    cardName: "card3",
+    questions: [
+      {label: "LABEL1", value: 1},
+      {label: "LABEL2", value: 2},
+      {label: "LABEL3", value: 3},
+      {label: "LABEL4", value: 4},
+    ],
+  },
+  {
+    typeOfInput: "radio",
+    cardLabel: "CARD 4 LABEL",
+    cardName: "card4",
+    questions: [
+      {label: "LABEL1", value: 1},
+      {label: "LABEL2", value: 2},
+      {label: "LABEL3", value: 3},
+      {label: "LABEL4", value: 4},
+    ],
+  },
+];
